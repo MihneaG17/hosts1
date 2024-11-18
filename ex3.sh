7cde8e8922b2f4016360b2f5095c4e33ff039198
MihneaG17 <mihnea298@gmail.com>
2024-11-18 19:24:33 +0200

    am adaugat fisierul

---
+++ ex3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat /etc/hosts | while read ip dom
+do
+	echo "ip: $ip, dom: $dom"
+	if [ $ip == "#" ]
+	then
+		break
+	fi
+	output=`nslookup $dom 8.8.8.8 | grep Address | awk '{print $2}' | tail -n 2 | head -n 1`
+ 	if [[ $output !=  $ip ]]
+        then
+         	 echo "Bogus IP for $dom in /etc/hosts !" 
+	fi
+done
+
